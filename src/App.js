d561cc77f0c8a5a8dec567c7db8f9b13345b6c61
Corneliu <corneliu.b@wavemyth.com>
2023-02-20 15:29:38 +0200

    customized index.html and added React Router Dom functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {Route, Routes} from 'react-router-dom'
+import CountriesPage from "./pages/CountriesPage";
+import HomePage from "./pages/HomePage";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/countries' element={<CountriesPage />} />
+        <Route path='*' element={<Page404 />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+import './styles/main.scss';
+import ThemeProvider from './contexts/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
