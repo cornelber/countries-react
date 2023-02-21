82b2d78b4e44a4459abce951b0283531b8522213
Cornel <bercornel@gmail.com>
2023-02-21 20:52:20 +0200

    updated theme toggler component

--- src/components/Buttons/ThemeTogglerButton.jsx
+++ src/components/Buttons/ThemeTogglerButton.jsx
@@ -2,16 +2,21 @@ import React, { useContext, useEffect } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
 const ThemeTogglerButton = () => {
-    const {isDarkTheme, toggleTheme} = useContext(ThemeContext)
+    const {isDarkTheme, toggleTheme} = useContext(ThemeContext);
 
-        // update body classname if theme is changed
-        useEffect(() => {
-            document.body.className = `theme-${isDarkTheme ? 'dark' : 'light'}`
-        }, [isDarkTheme])
+    // Update body classname if theme is changed
+    useEffect(() => {
+        document.body.className = `theme-${isDarkTheme ? 'dark' : 'light'}`;
+    }, [isDarkTheme]);
 
     return (
         <label className='toggler-label'>
-            <input className='toggler-input' type="checkbox" checked={isDarkTheme} onChange={toggleTheme}  />
+            <input 
+                className='toggler-input' 
+                type="checkbox" 
+                checked={isDarkTheme} 
+                onChange={toggleTheme}  
+            />
             <span className='toggler-icons-wrapper'>
                 <ion-icon name='sunny' />
                 <ion-icon name='moon' />
