31adea16aab47464e45fa19f0872a41fdffc8b77
Corneliu <corneliu.b@wavemyth.com>
2023-02-22 11:37:00 +0200

    created and styled Page404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import LayoutComponent from '../components/LayoutComponent'
 
 const Page404 = () => {
   return (
-    <div>Page404</div>
+    <LayoutComponent>
+      <div className="unknown-page">
+        <h2 className='unknown-title'>
+          404 - PAGE NOT FOUND
+        </h2>
+        <p className='unknown-description'>
+          The Page you are looking for may have been moved, deleted or possibly never exist
+        </p>
+        <Link className='link' to='/'>
+          Go to Homepage
+        </Link>
+      </div>
+    </LayoutComponent>
   )
 }
 
